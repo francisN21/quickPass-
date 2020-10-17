94ec324cfb64cc5306c0dc4e7d9732240c5f1128
Francisco Rones <ininorones@gmail.com>
2020-10-16 22:27:42 -0700

    First commit: html, js and css added

---
+++ app.js
@@ -0,0 +1,39 @@
+let chars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let Cchars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let nums = [1,2,3,4,5,6,7,8,9,0];
+let sChars = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+let emojis = [":)",":(",":-P","xD","UwU","¯\_(ツ)_/¯","(¬‿¬)","(• ε •)","(ノಠ益ಠ)ノ彡┻━┻","(づ￣ ³￣)づ","=U",""];
+
+// stores posible characters for the password
+var possibles = [];
+
+// parseInt makes the input into integer //
+var lenght = parseInt(prompt("How long do you want you password?"));
+
+if (length < 8 || length 128 || isNaN(length)){
+    // stops the program if the value is under and over //
+    alert("wrong input!");
+}
+// continue //
+else{
+    var charsTrue = confirm("Do you want Chars?");
+    var numsTrue = confirm("Do you want Chars?");
+    var sCharsTrue = confirm("Do you want Chars?");
+    var emojiTrue = confirm("Do you want Chars?");
+
+
+    if (charsTrue) possibles.push(chars)
+    if (numsTrue) possibles.push(nums)
+    if (sCharsTrue) possibles.push(sChars)
+    if (emojisTrue) possibles.push(emojis)
+
+    var pw = "";
+
+    while (pw.length < length){
+    for (let i = 0; i < possibles.length; i++){
+        let rand = math.floor(math.random () possibles[i].length);
+        pw += possibles[i][rand]
+        }
+        console.log(pw);
+    }
+}
